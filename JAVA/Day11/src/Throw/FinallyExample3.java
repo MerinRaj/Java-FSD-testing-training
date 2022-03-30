f2235039c5ccce4eaf270cbb8f47ec3d2aa7ef02
MerinRaj <48307317+MerinRaj@users.noreply.github.com>
2022-03-30 18:51:04 +0530

    Add files via upload

---
+++ JAVA/Day11/src/Throw/FinallyExample3.java
@@ -0,0 +1,24 @@
+package Throw;
+
+public class FinallyExample3 {
+
+	public static void main(String[] args) {
+		try
+		{
+			int data=25/0;
+			System.out.println("data="+data);
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println(e);
+		}
+		finally
+		{
+			System.out.println("Finally block is always executed");
+		}
+
+		System.out.println("Rest of the code");
+
+	}
+
+}
